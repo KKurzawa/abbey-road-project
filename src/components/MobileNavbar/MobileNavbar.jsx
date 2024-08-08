746fbce9d5cca7dfe345c2154a6962d591ca1085
Kris Kurzawa <kurzawa1@yahoo.com>
2024-08-08 13:46:53 -0400

    added hamburger

--- src/components/MobileNavbar/MobileNavbar.jsx
+++ src/components/MobileNavbar/MobileNavbar.jsx
@@ -1,9 +1,17 @@
 import './MobileNavbar.css'
+import { Spin as Hamburger } from 'hamburger-react'
+import { useState } from 'react'
 
 const MobileNavbar = () => {
+    const [isOpen, setOpen] = useState(false)
+
     return (
-        <main className='mobile-nav-main'>
-            <navbar className='mobile-nav'>Ham</navbar>
+        <main className='mobile-nav-main flex items-center'>
+            <Hamburger
+                toggled={isOpen}
+                toggle={setOpen}
+                direction="right"
+                size={35} />
         </main>
     )
 }
