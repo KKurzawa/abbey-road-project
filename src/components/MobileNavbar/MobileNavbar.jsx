95966e648a5fabce663e1a01655c783c75653226
Kris Kurzawa <kurzawa1@yahoo.com>
2024-08-09 11:40:33 -0400

    added links to mobile nav

--- src/components/MobileNavbar/MobileNavbar.jsx
+++ src/components/MobileNavbar/MobileNavbar.jsx
@@ -8,6 +8,30 @@ import { IoLogoYoutube } from "react-icons/io5";
 const MobileNavbar = () => {
     const [isOpen, setOpen] = useState(false)
 
+    function navigateToShows() {
+        setTimeout(() => {
+            setOpen(false)
+            const element = document.querySelector('#Shows');
+            element?.scrollIntoView({ behavior: 'smooth' })
+        }, 500)
+    }
+
+    function navigateToBand() {
+        setTimeout(() => {
+            setOpen(false)
+            const element = document.querySelector('#Band');
+            element?.scrollIntoView({ behavior: 'smooth' })
+        }, 500)
+    }
+
+    function navigateToContact() {
+        setTimeout(() => {
+            setOpen(false)
+            const element = document.querySelector('#Contact');
+            element?.scrollIntoView({ behavior: 'smooth' })
+        }, 500)
+    }
+
     return (
         <main className='mobile-nav-main flex items-center'>
             <Hamburger
@@ -19,9 +43,9 @@ const MobileNavbar = () => {
                 <nav className='absolute z-10 top-[3.3rem]'
                 >
                     <ul className='mobile-nav pb-4'>
-                        <li className='mobile-nav-link pl-2'>SHOWS</li>
-                        <li className='mobile-nav-link pl-2'>BAND</li>
-                        <li className='mobile-nav-link pl-2'>CONTACT</li>
+                        <li className='mobile-nav-link pl-2'><button className='w-fit' onClick={navigateToShows}>SHOWS</button></li>
+                        <li className='mobile-nav-link pl-2'><button className='w-fit' onClick={navigateToBand}>BAND</button></li>
+                        <li className='mobile-nav-link pl-2'><button onClick={navigateToContact} className='w-fit' >CONTACT</button></li>
                     </ul>
                     <ul className='nav-icons flex gap-6 pl-2 pb-6'>
                         <button className='nav-icon-btn' onClick={() => setTimeout(() => window.open('https://www.facebook.com/profile.php?id=61563049205575', '_blank'), 500)}><FaFacebookSquare className='nav-icon' /></button>
