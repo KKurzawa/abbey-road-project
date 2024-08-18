795ab911d9566ab5e04d089c7351c4361a963fe9
Kris Kurzawa <kurzawa1@yahoo.com>
2024-08-18 17:27:50 -0400

    started landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css'
 import Home from '../src/pages/Home/Home'
+import Landing from './pages/Landing/Landing';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
+
+  if (isLoading) {
+    return <Landing />
+  }
   return (
     <Home />
   )
---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,38 @@
+import './Landing.css'
+
+const Landing = () => {
+    return (
+        <main className='flex justify-center items-center w-full h-full mt-24'>
+            <article className='logo flex flex-col justify-center items-center gap-1'>
+                <article className='title-container flex flex-row justify-center gap-1'>
+                    <h2 className='landing-title'>T</h2>
+                    <h2 className='landing-title'>H</h2>
+                    <h2 className='landing-title'>E</h2>
+                </article>
+                <article className='title-container flex flex-row justify-center gap-1'>
+                    <h2 className='landing-title'>A</h2>
+                    <h2 className='landing-title'>B</h2>
+                    <h2 className='landing-title'>B</h2>
+                    <h2 className='landing-title'>E</h2>
+                    <h2 className='landing-title'>Y</h2>
+                    <h2 className='landing-space'>S</h2>
+                    <h2 className='landing-title'>R</h2>
+                    <h2 className='landing-title'>O</h2>
+                    <h2 className='landing-title'>A</h2>
+                    <h2 className='landing-title'>D</h2>
+                </article>
+                <article className='title-container flex flex-row justify-center gap-1'>
+                    <h2 className='landing-title'>P</h2>
+                    <h2 className='landing-title'>R</h2>
+                    <h2 className='landing-title'>O</h2>
+                    <h2 className='landing-title'>J</h2>
+                    <h2 className='landing-title'>E</h2>
+                    <h2 className='landing-title'>C</h2>
+                    <h2 className='landing-title'>T</h2>
+                </article>
+            </article>
+        </main>
+    )
+}
+
+export default Landing
