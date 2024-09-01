4c2a902f34dff7e9b4bee3dc2faf02f54a1406c1
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-01 15:42:39 -0400

    added instagram link

--- src/components/MobileNavbar/MobileNavbar.jsx
+++ src/components/MobileNavbar/MobileNavbar.jsx
@@ -46,7 +46,6 @@ const MobileNavbar = () => {
                 className='hamburger' />
             {isOpen &&
                 <motion.nav
-                    // onClick={toggleMenu}
                     transition={{ duration: .2, type: 'spring', stiffness: 50 }}
                     initial={{ top: '-15rem' }}
                     animate={{ top: '2.5rem' }}
@@ -77,7 +76,7 @@ const MobileNavbar = () => {
                         transition={{ duration: .05, delay: .7, }}
                         className='nav-icons flex gap-4 relative pl-2 pb-6'>
                         <button className='nav-icon-btn' onClick={() => setTimeout(() => window.open('https://www.facebook.com/profile.php?id=61563049205575', '_blank'), 500)}><FaFacebookSquare className='mobile-nav-icon' /></button>
-                        <button className='nav-icon-btn' onClick={() => setTimeout(() => window.open('https://www.instagram.com/k_kurzawa/', '_blank'), 500)}><FaInstagramSquare className='mobile-nav-icon' /></button>
+                        <button className='nav-icon-btn' onClick={() => setTimeout(() => window.open('https://www.instagram.com/the_abbey_road_project_detroit/', '_blank'), 500)}><FaInstagramSquare className='mobile-nav-icon' /></button>
                     </motion.ul>
                 </motion.nav>
             }
