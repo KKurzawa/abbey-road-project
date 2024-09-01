b4fbc31dc34449a74da21780f71775315eee40b3
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-01 14:19:24 -0400

    spell checked bios

--- src/components/Utils/Bios.jsx
+++ src/components/Utils/Bios.jsx
@@ -1,7 +1,7 @@
 const memberBios = [
     {
         name: 'Michael King',
-        bio: "Michael King is a Detroit based multi-instrumentalist Songwriter, Producer and Composer. His music blends Contemporary, Progressive, Folk and World Beat elements with detailed production, emotive instrumental landscapes and literary lyrics in to a sound that has wide appeal.  Michael was a Founding member of the Atlantic Recording Group Rebel Heels and has released 4 solo albums: Glory, Motion, A Thousand Faces and Nevada.  He is now the Music Director at Kensington Church."
+        bio: "Michael King is a Detroit-based multi-instrumentalist Songwriter, Producer and Composer. His music blends Contemporary, Progressive, Folk, and World Beat elements with detailed production, emotive instrumental landscapes, and literary lyrics into a sound that has wide appeal. Michael was a Founding member of the Atlantic Recording Group Rebel Heels and has released 4 solo albums: Glory, Motion, A Thousand Faces, and Nevada. He is now the Music Director at Kensington Church."
     },
     {
         name: 'Doug Brown',
@@ -9,15 +9,15 @@ const memberBios = [
     },
     {
         name: 'Dave Henderickson',
-        bio: "Dave is a musician, singer, songwriter and producer.  He’s been described as “a bass player’s bass player”.  Artists hire him again and again for his professionalism.  From his earliest days picking up a bass in school and playing along with every Top 40 song on the radio through his time at the Berklee College of Music and Oakland University jazz departments, Dave has honed his skills in all styles of music from Big Band and Funk to Country, Jazz and more."
+        bio: "Dave is a musician, singer, songwriter, and producer.  He’s been described as “a bass player’s bass player”.  Artists hire him again and again for his professionalism.  From his earliest days picking up a bass in school and playing along with every Top 40 song on the radio through his time at the Berklee College of Music and Oakland University jazz departments, Dave has honed his skills in all styles of music from Big Band and Funk to Country, Jazz and more."
     },
     {
         name: 'Kris Kurzawa',
-        bio: "Kris Kurzawa’s versatility has led him to become one of Detroit area’s most in demand guitarists since the early 2000’s; crossing multiple genres including jazz, fusion, rock, world music and R+B.  He released his first album ‘Sly’ in 2016 earning him his fifth Detroit Music Award.  He earned his bachelors degree in 2003 and his masters degree in 2012 with a concentration in jazz studies from Wayne State University where his now adjunct faculty in their jazz studies program."
+        bio: "Kris Kurzawa’s versatility has led him to become one of the Detroit area’s most in-demand guitarists since the early 2000s; crossing multiple genres including jazz, fusion, rock, world music, and R+B.  He released his first album ‘Sly’ in 2016 earning him his fifth Detroit Music Award.  He earned his bachelor's degree in 2003 and his master's degree in 2012 with a concentration in jazz studies from Wayne State University where his now adjunct faculty in their jazz studies program."
     },
     {
         name: 'Steve Nolton',
-        bio: "Steve Nolton is a drummer/percussionist, producer and composer who has been performing in the Detroit music scene spanning several decades. He is the founding member of the Abbey Road Project, the Mask project and SWL Stevie’s Wonders LiVE! Steve is known for his solid groove, and his ability to play many different styles of music. He is the backbone of The Simone Vitale Band - one of Detroit’s Premier High Society Party Bands."
+        bio: "Steve Nolton is a drummer/percussionist, producer, and composer who has been performing in the Detroit music scene spanning several decades. He is the founding member of the Abbey Road Project, the Mask Project, and SWL Stevie’s Wonders LiVE! Steve is known for his solid groove, and his ability to play many different styles of music. He is the backbone of The Simone Vitale Band - one of Detroit’s Premier High Society Party Bands."
     },
 ]
 
